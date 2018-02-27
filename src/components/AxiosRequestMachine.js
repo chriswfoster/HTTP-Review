8134e93a6a1f087aefdafd20a01afe51fe4e664c
ChriswFoster <chriswfoster@gmail.com>
2018-02-27 11:37:07 -0600

    Created axios component

---
+++ src/components/AxiosRequestMachine.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class AxiosRequestMachine extends Component{
+
+
+
+    render(){
+        return (
+            <div>
+                Test
+            </div>
+        )
+    }
+}
+export default AxiosRequestMachine 
